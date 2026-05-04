547325c393233d71fdb2856d204572e8a91a1d7b
MD UMAR HUSSAIN <115338236+M-hussai@users.noreply.github.com>
2026-05-05 04:23:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-// Fix for Error 1: Close Button
-it('should close modal when close button is clicked', () => {
-  cy.get('#openModal').click();
-  cy.get('.close-modal').click();
-  
-  // DO NOT use .have.css('display', 'none')
-  // Use 'not.be.visible' - it waits for the change to happen
-  cy.get('#myModal').should('not.be.visible'); 
-});
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("openModal");
+const span = document.getElementsByClassName("close-modal")[0];
 
-// Fix for Error 2: Click Outside
-it('should close modal when clicking outside of modal', () => {
-  cy.get('#openModal').click();
-  
-  // Wait for it to be visible first
-  cy.get('#myModal').should('be.visible');
+// Open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
-  // Click the top-left corner of the screen (the background overlay)
-  cy.get('#myModal').click(0, 0, { force: true });
+// Close when (x) is clicked
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-  cy.get('#myModal').should('not.be.visible');
-});
+// Close when clicking OUTSIDE the white box
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
